add2d5abbb893c7cf0549cebfa4a0eef3ec4ca66
LenGo-Dev <lena.gotovska@gmail.com>
2025-07-25 11:58:13 +0200

    Add variable playerMove

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let computerMove = `kamień`;
+let playerMove = `papier`;
+
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
